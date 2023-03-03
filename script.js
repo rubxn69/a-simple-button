58e8c8126572cc5be6e7b74b276b42f71d1a5e42
Ruben <103433618+rubxn69@users.noreply.github.com>
2023-03-03 22:04:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let button1 = document.getElementById('btn')
+button1.addEventListener('click', () => {
+    alert("poi chakada myre")
+    console.log("clicked")
+    button1.textContent = "ini poi chatho"
+})
